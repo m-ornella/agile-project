5c999dc2ad1d176f326ca55674a7d0c164478f04
LouisBrousse <brousselouis@hotmail.com>
2026-03-11 11:48:39 +0100

    Interface play panel

---
+++ src/game/yamsTurnState.js
@@ -0,0 +1,60 @@
+import { lancerDes } from '../service/lancerDes'
+
+const DICE_COUNT = 5
+const MAX_ROLLS = 3
+
+export class YamsTurnState {
+  constructor() {
+    this.reset()
+  }
+
+  reset() {
+    this.diceValues = Array(DICE_COUNT).fill(null)
+    this.selectedForReroll = Array(DICE_COUNT).fill(false)
+    this.rollsUsed = 0
+  }
+
+  get rollsRemaining() {
+    return MAX_ROLLS - this.rollsUsed
+  }
+
+  canSelectDice() {
+    return this.rollsUsed > 0 && this.rollsRemaining > 0
+  }
+
+  toggleDieSelection(index) {
+    if (!this.canSelectDice()) return
+    if (index < 0 || index >= DICE_COUNT) return
+
+    this.selectedForReroll[index] = !this.selectedForReroll[index]
+  }
+
+  getSelectedDiceCount() {
+    return this.selectedForReroll.filter(Boolean).length
+  }
+
+  roll() {
+    if (this.rollsRemaining <= 0) {
+      throw new Error('Aucun lancer restant pour ce tour.')
+    }
+
+    const indicesToRoll = this.rollsUsed === 0
+      ? Array.from({ length: DICE_COUNT }, (_, index) => index)
+      : this.selectedForReroll
+          .map((isSelected, index) => (isSelected ? index : -1))
+          .filter(index => index !== -1)
+
+    if (indicesToRoll.length === 0) {
+      throw new Error('Selectionnez au moins un de a relancer.')
+    }
+
+    const rollResults = lancerDes(indicesToRoll.length)
+
+    indicesToRoll.forEach((dieIndex, resultIndex) => {
+      this.diceValues[dieIndex] = rollResults[resultIndex]
+    })
+
+    this.rollsUsed += 1
+    this.selectedForReroll.fill(false)
+  }
+}
